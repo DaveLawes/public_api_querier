4b39ca02762141a838909c2586eb18b8076cab73
David Lawes <davidlawes@hotmail.co.uk>
2018-11-10 15:10:53 +0000

    test: feature test for total_spend

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -5,6 +5,11 @@ describe 'app.rb' do
     expect(most_sold).to eq "Heavy Duty Concrete Watch\n"
   end
 
+  it 'returns the total spend for a given email' do
+    total_spend = %x(ruby app.rb total_spend schimmel_quincy@ernser.io)
+    expect(total_spend).to eq "245.01\n"
+  end
+
   it 'returns a message when unknown question is asked' do
     unknown_question = %x(ruby app.rb xxx)
     expect(unknown_question).to eq "Sorry, unknown question\n"
