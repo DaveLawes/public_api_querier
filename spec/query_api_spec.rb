9b7e9eeb268066b2c7ac0705ca76a12b8d1a11e0
David Lawes <davidlawes@hotmail.co.uk>
2018-11-10 17:17:49 +0000

    test: edge case for HTTP response not successful

--- spec/query_api_spec.rb
+++ spec/query_api_spec.rb
@@ -3,6 +3,7 @@ require 'query_api'
 describe QueryApi do
 
   response = Net::HTTPSuccess.new(1.0, '200', 'OK')
+  unsuccessful_response = Net::HTTPClientError.new(1.0, '404', 'NOT OK')
   let(:http) { double :http_double, get_response: response }
   subject { described_class.new(http) }
 
@@ -20,12 +21,23 @@ describe QueryApi do
       subject.all_purchases
     end
 
-    it 'returns the body of the response in json format' do
-      http_body = JSON.parse(purchases)
-      expected_return = http_body['data']
+    context 'given HTTP response is successful' do
+      it 'returns the body of the response in json format' do
+        http_body = JSON.parse(purchases)
+        expected_return = http_body['data']
 
-      expect(subject.all_purchases).to eq expected_return
+        expect(subject.all_purchases).to eq expected_return
+      end
     end
+
+    context 'given the HTTP response is not successful' do
+      it 'raises an error' do
+        allow(http).to receive(:get_response).and_return(unsuccessful_response)
+
+        expect { subject.all_purchases }.to raise_error('HTTP response was not successful')
+      end
+    end
+
   end
 
   describe '#all_users' do
