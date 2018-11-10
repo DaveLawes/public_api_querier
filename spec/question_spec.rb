42719974553983b516367f2b111e4c1e8ed53bce
David Lawes <davidlawes@hotmail.co.uk>
2018-11-10 14:11:39 +0000

    test: added unit test for Question.most_sold

--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -3,6 +3,7 @@ require 'question'
 describe Question do
 
   let(:query_api) { double :query_api_double, all_purchases: '', all_users: '' }
+  subject { described_class.new(query_api) }
 
   describe '#init' do
     it 'requests all purchases from query_api' do
@@ -15,6 +16,12 @@ describe Question do
       expect(query_api).to receive(:all_users)
 
       Question.new(query_api)
+    end
+  end
+
+  describe '#most_sold' do
+    it 'returns the most sold item' do
+      expect(subject.most_sold).to eq 'Heavy Duty Concrete Watch'
     end
   end
 
