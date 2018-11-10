0b1740b6e9629eab8cdb886b8cbbc6f0a7ae339c
David Lawes <davidlawes@hotmail.co.uk>
2018-11-10 14:38:43 +0000

    test: unit test passing for query_api.all_purchases

--- lib/query_api.rb
+++ lib/query_api.rb
@@ -1,4 +1,14 @@
 require 'net/http'
 
 class QueryApi
+
+  def initialize(http = Net::HTTP)
+    @http = http
+  end
+
+  def all_purchases
+    purchases_uri = URI('https://driftrock-dev-test.herokuapp.com/purchases')
+    response = @http.get_response(purchases_uri)
+  end
+
 end
