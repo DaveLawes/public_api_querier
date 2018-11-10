51881f5e0f8f35bb304341592e9582e4f5db7549
David Lawes <davidlawes@hotmail.co.uk>
2018-11-10 14:22:25 +0000

    test: unit test for question.most_sold passing

--- lib/question.rb
+++ lib/question.rb
@@ -5,4 +5,13 @@ class Question
     @all_users = query_api.all_users
   end
 
+  def most_sold
+    all_items = @all_purchases.map { |element| element['item'] }
+    mapped = all_items.each_with_object(Hash.new(0)) do |element, value|
+      value[element] += 1
+    end
+    sorted = mapped.sort_by { |k, v| v }.reverse
+    sorted[0][0]
+  end
+
 end
